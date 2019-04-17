35cda7eb4e23ca1ae8e05efb45188964a7458e72
ycjcl868 <45808948@qq.com>
2019-04-17 08:10:01 +0800

    feat: support hover value show token

--- src/HoverProvider.ts
+++ src/HoverProvider.ts
@@ -8,15 +8,38 @@ export default class implements vscode.HoverProvider {
     this.themeConfig = themeConfig;
     console.log('----this.themeConfig--', this.themeConfig);
   }
+  getTip = (token: string, prefix?: string) => {
+    if (!this.themeConfig) {
+      return null;
+    }
+    const themeIterator = this.themeConfig.entries();
+    const firstLetter = prefix || '';
+    for (let theme of themeIterator) {
+      const [key, config] = theme;
+      // console.log('-----key-', key, prefix, config.value, `${firstLetter}${token}`);
+      // key => value
+      if (key === token) {
+        return config;
+      }
+      // value => key
+      if (config.value === `${firstLetter}${token}`) {
+        return {
+          value: `@${key}`,
+          comment: config.comment,
+        };
+      }
+    }
+  }
 	async provideHover(
 		doc: vscode.TextDocument,
 		pos: vscode.Position,
 	) {
-		const range = doc.getWordRangeAtPosition(pos, /@[\w-]+/);
+		const range = doc.getWordRangeAtPosition(pos, /(#|@)?[\w-]+/);
 		if (range) {
-			const token = doc.getText(new vscode.Range(range.start.translate(0, 1), range.end));
-			console.log('--token--', token);
-			const tip = this.themeConfig && this.themeConfig.get(token);
+      const prefixRange = range.start.translate(0, 1);
+      const prefix = doc.getText(new vscode.Range(prefixRange, range.start));
+			const token = doc.getText(new vscode.Range(prefixRange, range.end));
+			const tip = this.getTip(token, prefix);
 			if (tip) {
 				return new vscode.Hover(renderTip(tip));
 			}
